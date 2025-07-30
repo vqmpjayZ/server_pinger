b2c7d4be7fc46142fc9062475656b00841e327c6
vqmpjay <bakirbutkovic13@gmail.com>
2025-07-30 20:36:00 +0200

    Update server-a.js

--- server-a.js
+++ server-a.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const targets = [
   "https://converttopixel-esnq.onrender.com",
   "https://59955d91-8de9-4508-9086-50d814b6421d-00-23am6j7qehoyy.kirk.replit.dev/",
-  // "",
+  "https://7bf316d4-562e-4bed-b2bc-fbb94c805f2c-00-2k4z7h6j1re0i.worf.replit.dev/",
 ];
 
 function startPinging() {
